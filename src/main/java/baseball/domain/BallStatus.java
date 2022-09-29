3a1657011aa164c216ce23e3482788ffe4a57973
sixthou <justyuk@naver.com>
2022-09-29 13:37:46 +0900

    feat : BallStatus 상태확인 메서드 추가

--- src/main/java/baseball/domain/BallStatus.java
+++ src/main/java/baseball/domain/BallStatus.java
@@ -4,5 +4,15 @@ public enum BallStatus {
 
     STRIKE,
     BALL,
-    NOTHING
+    NOTHING;
+
+    public boolean isStrike() {
+        return this == STRIKE;
+    }
+
+    public boolean isBall() {
+        return this == BALL;
+    }
+
 }
+
